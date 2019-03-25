7210d42529471ca5be3362109d6f564eb53ededf
Aqurds <omarf1320@gmail.com>
2019-03-26 03:47:01 +0600

    asin search app heroku version

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# Display route
+@app.route('/result/')
+def display():
+    return render_template('result.html')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
